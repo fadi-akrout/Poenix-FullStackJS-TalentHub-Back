eea3fea21f5912053b8ff0a2135ead02898f0a12
sarrabs22 <sarra.bensedrine@sprit.tn>
2024-03-06 10:10:47 +0100

    pdf

--- routes/CandidateRoute.js
+++ routes/CandidateRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Candidate = require('../models/Candidate');
 
-router.post('/', async (req, res) => {
+router.post('/', async(req, res) => {
     try {
         const candidate = new Candidate(req.body);
         await candidate.save();
@@ -12,7 +12,20 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.get('/', async (req, res) => {
+router.get('/last', async(req, res) => {
+    try {
+        // Assuming there's a createdAt or similar timestamp field in your Candidate schema
+        const lastCandidate = await Candidate.findOne().sort({ createdAt: 1 });
+        if (!lastCandidate) {
+            return res.status(404).send({ message: 'No candidates found' });
+        }
+        res.status(200).send(lastCandidate);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.get('/', async(req, res) => {
     try {
         const candidates = await Candidate.find();
         res.send(candidates);
@@ -21,7 +34,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async(req, res) => {
     try {
         const candidate = await Candidate.findById(req.params.id);
         if (!candidate) {
@@ -33,7 +46,7 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', async(req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'lastname', 'email', 'diploma', 'actualPost', 'nbrYearsOfExperience', 'lastPostOccupied', 'cv', 'dateOfBirth', 'address', 'city', 'postalCode', 'country', 'phoneNumber', 'skills', 'languages', 'certifications', 'linkedinProfile'];
     const isValidOperation = updates.every(update => allowedUpdates.includes(update));
@@ -53,7 +66,7 @@ router.patch('/:id', async (req, res) => {
     }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async(req, res) => {
     try {
         const candidate = await Candidate.findByIdAndDelete(req.params.id);
         if (!candidate) {
