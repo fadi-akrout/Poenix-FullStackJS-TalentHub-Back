d76c98669800c6783e51416a61311af0d1c3d327
laroussiamir01 <amir.laroussi@esprit.tn>
2024-03-26 21:13:08 +0100

    forgetpassword1

--- models/User.js
+++ models/User.js
@@ -1,149 +1,9 @@
- //const mongoose = require('mongoose');
- /* 
-  */
-
-
-
- // const userSchema = new Schema({
- //   Id:Number,
- //   FirstName: String,
- //   LastName: String,
- //   PhoneNumber: Number,
- //   Diploma: String,
- //   LastPostOccupied: String,
- //   NbrOfExperience: Number,
-
- //   email: {
- //     type: String, 
- //     required: true,
- //     unique: true
- //   },
- //   password: {
- //     type: String,
- //     required: true
- //   },
- //   roles: [{
- //     type: String,
- //     default: "Employee"
- // }],
- //  /*  role: {
- //     type: String,
- //     enum: ['admin', 'recruiter', 'student', 'alumni', 'satff'],
- //     default: 'user' 
- //   }, */
-
- //   active: {
- //     type: Boolean,
- //     default: true 
- //   }
-
-
-
-
- // })
-
-
-
- // // static signup method ======================
- // userSchema.statics.signup = async function(email,password) {
- //   //validation
- //   if(!email || !password) { 
- //     throw new Error('Email or Password not provided') 
- //   }
- //   if (!validator.isEmail(email)) {
- //     throw new Error("Invalid Email")
- //   }
- //   if(!validator.isStrongPassword(password) ) {
- //     throw new Error("Password not strong enough")
- //   }
-
- //   const exists= await this.findOne({email})
- //   if(exists) {
- //     throw new Error('Email already in use')
- //   }
- //     const salt = await bcrypt.genSalt(10);
- //     const hash = await bcrypt.hash(password, salt)
-
- //     const user=await this.create({email,password:hash})
- //     return user;
- // }
- // // end of static signup method ====================== 
- // // static login method ====================== 
- //   userSchema.statics.login = async function (email, password) {
- //     if(!email || !password) { 
- //       throw new Error('Email or Password not provided') 
- //     }
- //     const user= await this.findOne({email})
- //     if(!user) {
- //       throw new Error('Incorrect Email')
- //     }
- //     if(!user.active) {
- //       throw new Error('Banned Account')
- //     }
- //     const match= await bcrypt.compare(password, user.password)
- //     if(!match) {
- //         throw new Error('Incorrect Password')
- //     }
-
- //     return user;
- //   }
- // // end of static login method ====================== 
- // //
-
-
- // const User = mongoose.model('User', userSchema);
-
-
- // module.exports = User;
-
- const mongoose = require('mongoose')
- const bcrypt = require('bcrypt')
+ const mongoose = require('mongoose');
+ const bcrypt= require('bcrypt')
  const validator = require('validator')
  const Schema = mongoose.Schema;
 
 
-<<<<<<< HEAD
- const userSchema = new Schema({
-     Id: Number,
-     name: String,
-     email: {
-         type: String,
-         required: true,
-         unique: true
-     },
-     password: {
-         type: String,
-         required: true
-     },
-     roles: {
-         type: String,
-         enum: ['Admin', 'Recruter', 'Student', 'Teacher', 'Alumni'],
-         default: 'Student' // Default role is Student if not provided
-     },
-     active: {
-         type: Boolean,
-         default: true
-     },
-     resetPasswordToken: String,
-     resetPasswordExpires: Date,
- })
-
-
-
- // static signup method ======================
- userSchema.statics.signup = async function(email, password) {
-         //validation
-         if (!email || !password) {
-             throw new Error('Email or Password not provided')
-         }
-         if (!validator.isEmail(email)) {
-             throw new Error("Invalid Email")
-         }
-         if (!validator.isStrongPassword(password)) {
-             throw new Error("Password not strong enough")
-         }
-
-=======
 const userSchema = new Schema({
   username: {
     type: String
@@ -153,7 +13,6 @@ const userSchema = new Schema({
          required: true,
         // unique: true
        },
-
   password: {
       type: String,
       required: true
@@ -173,7 +32,6 @@ const userSchema = new Schema({
     default: false
   }
                  })
-
 
 
  
@@ -226,43 +84,9 @@ userSchema.methods.comparePassword= async function(password){
   return await bcrypt.compareSync(password,this.password)
 }
  
->>>>>>> 1116c86ce88a5c4201971b889287e5c20be6790f
 
-         const exists = await this.findOne({ email })
-         if (exists) {
-             throw new Error('Email already in use')
-         }
-         const salt = await bcrypt.genSalt(10);
-         const hash = await bcrypt.hash(password, salt)
-
-         const user = await this.create({ email, password: hash })
-         return user;
-     }
-     // end of static signup method ====================== 
-     // static login method ====================== 
- userSchema.statics.login = async function(email, password) {
-         if (!email || !password) {
-             throw new Error('Email or Password not provided')
-         }
-         const user = await this.findOne({ email })
-         if (!user) {
-             throw new Error('Incorrect Email')
-         }
-         if (!user.active) {
-             throw new Error('Banned Account')
-         }
-         const match = await bcrypt.compare(password, user.password)
-         if (!match) {
-             throw new Error('Incorrect Password')
-         }
-
-         return user;
-     }
-     // end of static login method ====================== 
-     //
+const User = mongoose.model('User', userSchema);
 
 
- const User = mongoose.model('User', userSchema);
+module.exports = User;
 
-
- module.exports = User;
