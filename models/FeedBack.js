68cff8938829fcd8a62080d54d5993595af1d227
Hamza Tounsi <tounsi.hamza@esprit.tn>
2024-05-02 23:46:18 +0100

    fixe feedback

--- models/FeedBack.js
+++ models/FeedBack.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose');
 
 const feedbackSchema = new mongoose.Schema({
-  offerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Reference to the User model
-    required: true,
-  },
-  rating: {
-    type: Number,
-    required: true,
-  },
+    offerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    }
 });
 
 const Feedback = mongoose.model('Feedback', feedbackSchema);
