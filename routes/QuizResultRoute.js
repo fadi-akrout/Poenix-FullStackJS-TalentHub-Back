2156eaedc4ae2279f249ac684657a67cd904ffc2
laroussiamir01 <amir.laroussi@esprit.tn>
2024-05-02 14:38:10 +0200

    quizresult

--- routes/QuizResultRoute.js
+++ routes/QuizResultRoute.js
@@ -39,4 +39,21 @@ router.post('/quiz-results/latest', async(req, res) => {
         res.status(500).json({ message: 'Server error' }); // Send server error status if an exception occurs
     }
 });
+router.get('/quiz-results', async (req, res) => {
+    const { userId, offerId } = req.query; // Accessing userId and offerId from the query parameters
+  
+    try {
+      const results = await QuizResult.find({ user: userId, offer: offerId });
+  
+      if (results.length > 0) {
+        res.json(results); // Send the results if found
+      } else {
+        res.status(404).json({ message: 'No results found' }); // Send 404 if no results
+      }
+    } catch (error) {
+      console.error('Failed to fetch quiz results:', error);
+      res.status(500).json({ message: 'Server error' }); // Send server error status if an exception occurs
+    }
+  });
+
 module.exports = router;
